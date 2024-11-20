7c57603308b14635f37ec06521eebc01b097f17e
Dylon Edwards <dylon.devo@gmail.com>
2024-11-19 21:22:57 -0500

    Adds separate lfortran test script for Windows

--- server/test/spec/lfortran-common.ts
+++ server/test/spec/lfortran-common.ts
@@ -1,10 +1,15 @@
 import path from "path";
+import * as os from "os";
 
 import { ExampleSettings } from "../../src/lfortran-types";
+
+const scriptName: string = ((os.platform() === "win32")
+  ? "lfortran.ps1"
+  : "lfortran.sh");
 
 export const settings: ExampleSettings = {
   maxNumberOfProblems: 100,
   compiler: {
-    lfortranPath: path.join(__dirname, "..", "bin", "lfortran")
+    lfortranPath: path.join(__dirname, "..", "bin", scriptName)
   }
 };
